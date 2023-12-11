32f9acae75618bfb7cde8d9dbc9ec96bbef46c82
booba <azarenkoartuom@mail.ru>
2023-12-11 22:47:13 +0300

    project created

---
+++ src/main/java/by/softech/softmarket/SoftmarketApplication.java
@@ -0,0 +1,13 @@
+package by.softech.softmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftmarketApplication.class, args);
+	}
+
+}
